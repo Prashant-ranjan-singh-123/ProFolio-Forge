daaf4519309f555d9ad70071a849d7325223ad77
Prashant-ranjan-singh-123 <prashant.singh.12312345@gmail.com>
2024-06-13 02:25:26 +0530

    Running App

---
+++ lib/shared/app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:get/get.dart';
+import 'package:iconly/iconly.dart';
+
+class MyAppBar {
+  static AppBar customAppBar({required String title}) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      centerTitle: true,
+      title: Builder(
+        builder: (context) => Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: const Icon(
+                IconlyLight.filter,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Get.back();
+              },
+            ),
+            Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.w900,
+                fontFamily: 'Poppins',
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(),
+          ],
+        ),
+      ),
+      backgroundColor: const Color.fromRGBO(82, 59, 153, 0.2),
+    );
+  }
+}
